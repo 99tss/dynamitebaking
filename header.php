0067cdc9830369fd63f48f7189ef0ebbec7c4e4e
99tss <Thomas@Thomass-MacBook-Pro.local>
2014-07-28 11:56:45 +0100

    Dynamite Baking

---
+++ header.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<h1>Dynamite Baking</h1>
+<h4><sup>by</sup></h4>
+<h5><sup>T</sup></h5>
+<h6><sup>n</sup></h6>
+<h7><sup>T</sup></h7>
+
+</head>
+
+<body>
+<h3>
+<ul>
+<li><a href="DynamiteBakingIMPROVED.html">Home</a></li>
+<li><a href="DynamiteBaking - Recipe Page.html">Recipes</a></li>
+<li><a href="DynamiteBaking - Baking Page.html">Baking tips</a></li>
+<li><a href="#About">About</a></li>
+</ul>
+</h3>
+</body>
+<head>
+<link href='http://fonts.googleapis.com/css?family=Englebert' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<h2><font color="#696969"> The Best Cakes, Biscuits & Pastry recipes we can find</h2>
+<h2><font color="#FF5050">Compare recipes from Top Cooks and Chefs from around the World</font></h2>
+<h3><font color="">Ingredients Price included</font></h3>
